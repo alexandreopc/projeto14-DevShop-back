3766ae69bd3aa057a5c11ae4751244a245506c36
unverzed <vitoriarortega@gmail.com>
2022-05-13 19:53:51 -0300

    feat: get for navpage audio

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -10,13 +10,28 @@ export async function getProducts(req, res) {
   }
 }
 
-export async function getHardware(req, res){
-  try{
-    const hardware = await db.collection("products").find({category: "hardware"}).toArray();
+export async function getHardware(req, res) {
+  try {
+    const hardware = await db
+      .collection("products")
+      .find({ category: "hardware" })
+      .toArray();
     res.send(hardware);
-  }
-  catch(e){
+  } catch (e) {
     res.sendStatus(500);
     console.log("erro ao pegar os itens de hardware", e);
+  }
+}
+
+export async function getAudio(req, res) {
+  try {
+    const audio = await db
+      .collection("products")
+      .find({ category: "audio" })
+      .toArray();
+    res.send(audio);
+  } catch (e) {
+    res.sendStatus(500);
+    console.log("erro ao pegar os itens de audio", e);
   }
 }
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getProducts, getHardware } from "./../controllers/productsController.js";
+import { getProducts, getHardware, getAudio } from "./../controllers/productsController.js";
 
 const productsRouter = Router();
 
 productsRouter.get("/home", getProducts);
 productsRouter.get("/hardware", getHardware);
+productsRouter.get("/audio", getAudio);
 
 export default productsRouter;
